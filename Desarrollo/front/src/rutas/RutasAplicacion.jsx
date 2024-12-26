203a859f9c41704bd92ae2804ef3065e1c751d80
m1chaelj <jahicco24@gmail.com>
2024-12-25 23:58:09 -0600

    dia terminado :)

--- Desarrollo/front/src/rutas/RutasAplicacion.jsx
+++ Desarrollo/front/src/rutas/RutasAplicacion.jsx
@@ -7,15 +7,17 @@ import RegistroSinodal from '../paginas/RegistroSinodal'; // Registro de sinodal
 import RegistroDirector from '../paginas/RegistroDirector'; // Registro de director
 import RecuperarContrasena from '../paginas/RecuperarContrasena';
 import RegistroProtocolo from '../paginas/RegistroProtocolo';
+import { Navigate } from 'react-router-dom';
 
 const RutasAplicacion = () => {
   return (
     <Routes>
-      <Route path="/" element={<InicioSesion />} />
-      <Route path="/registro" element={<RegistroSelector />} /> {/* Selector de perfil */}
-      <Route path="/registro/estudiante" element={<RegistroEstudiante />} /> {/* Formulario para estudiante */}
-      <Route path="/registro/sinodal" element={<RegistroSinodal />} /> {/* Formulario para sinodal */}
-      <Route path="/registro/secretario" element={<RegistroDirector />} /> {/* Formulario para director */}
+      <Route path="/" element={<Navigate to="/inicio" />} /> {/* Redirección automática */}
+      <Route path="/inicio" element={<InicioSesion />} />
+      <Route path="/registro" element={<RegistroSelector />} />
+      <Route path="/registro/estudiante" element={<RegistroEstudiante />} />
+      <Route path="/registro/sinodal" element={<RegistroSinodal />} />
+      <Route path="/registro/secretario" element={<RegistroDirector />} />
       <Route path="/recuperar-contrasena" element={<RecuperarContrasena />} />
       <Route path="/registro-protocolo" element={<RegistroProtocolo />} />
     </Routes>
