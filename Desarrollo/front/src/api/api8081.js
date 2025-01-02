95e0f27183e186d580fd514ac7e772eba5b943d6
m1chaelj <jahicco24@gmail.com>
2025-01-01 18:09:08 -0600

    Cambos hasta Penultima pantalla alumno

--- Desarrollo/front/src/api/api8081.js
+++ Desarrollo/front/src/api/api8081.js
@@ -6,36 +6,20 @@ const api8081 = axios.create({
   timeout: 10000, // Tiempo máximo para una solicitud
 });
 
-// Función para enviar `multipart/form-data`
-
-api8081.sendFormData = async (url, data) => {
-  const isFormData = data instanceof FormData;
-
+// Función para enviar multipart/form-data
+api8081.sendFormData = async (url, formData) => {
   try {
-    const headers = {
-      "Content-Type": isFormData ? "multipart/form-data" : "application/json"
-    };
-
-    const body = isFormData ? data : JSON.stringify(data);
-
-    const response = await fetch(url, {
-      method: "POST",
-      headers: headers,
-      body: body
+    const response = await api8081.post(url, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
     });
-
-    // Verifica la respuesta
-    if (response.ok) {
-      return await response.json(); // O usa .text() si no es JSON
-    } else {
-      throw new Error("Error en la solicitud");
-    }
+    return response;
   } catch (error) {
-    console.error("Error al enviar datos:", error);
+    console.error("Error al enviar FormData:", error);
     throw error;
   }
 };
-
 
 // Función para realizar solicitudes GET
 api8081.getData = async (url, params = {}) => {
@@ -48,4 +32,4 @@ api8081.getData = async (url, params = {}) => {
   }
 };
 
-export default api8081;
+export default api8081; 
